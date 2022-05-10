eff3449699e6ce688b679854b373afdca4c35298
Julianmonlon <julianmonlon@gmail.com>
2022-05-10 09:05:31 -0500

    Boiler plate

---
+++ juegoLenguajes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace juegoLenguajes
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
